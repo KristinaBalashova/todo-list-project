ab1b7f998ba6d31921285ce7a1ddcd3ad34598e5
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-11-12 23:35:40 +0100

    Add store and todo module

---
+++ src/store/modules/todos.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const state = () => ({
+  todos: [],
+  filter: 'all', //options: active, completed, all (probably bette use typescript)
+});
+
+const mutations = {
+  setTodos(state, todo) {
+    if (Array.isArray(todo)) {
+      state.todos = [...todo, ...state.todos];
+    } else {
+      state.todos = [todo, ...state.todos];
+    }
+  },
+  setFilter(state, filter) {
+    state.filter = filter;
+  },
+  removeCompletedTodos(state) {
+    state.todos = state.todos.filter((todo) => !todo.completed);
+  },
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    try {
+      const response = await axios('https://jsonplaceholder.typicode.com/users/1/todos?_limit=5');
+      commit('setTodos', response.data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  },
+  clearCompleted({ commit }) {
+    commit('removeCompletedTodos');
+  },
+  setFilter({ commit }, filter) {
+    commit('setFilter', filter);
+  },
+};
+
+const getters = {
+  todos: (state) => state.todos,
+  activeTodos: (state) => state.todos.filter((todo) => !todo.completed),
+  completedTodos: (state) => state.todos.filter((todo) => todo.completed),
+  filter: (state) => state.filter,
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters,
+};
