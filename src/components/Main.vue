815ded1f6a04980e013ddd5e152fb16a454a9291
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-11-12 23:47:37 +0100

    Add todos manager container

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,13 @@
 <script setup>
 import TodoList from './TodoList.vue';
 //import AddTodo from './AddTodo.vue';
-//import ManageTodo from './ManageTodo.vue';
+import TodosManager from './TodosManager.vue';
+
 </script>
 
 <template>
   {{ /* <AddTodo />*/ }}
-  {{ /* <ManageTodo />*/ }}
+  <TodosManager />
   <TodoList />
 </template>
 
---
+++ src/components/TodosManager.vue
@@ -0,0 +1,59 @@
+<script>
+import Filters from './Filters.vue';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'TodosManager',
+  components: {
+    Filters,
+  },
+  computed: {
+    ...mapGetters('todos', ['todos']),
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="actions">
+      <button class="button">Активные: {{ todos.length }}</button>
+      <button class="button">Удалить завершенные</button>
+    </div>
+
+    <Filters />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 20px;
+}
+
+.actions {
+  display: flex;
+  gap: 10px;
+}
+
+.button {
+  padding: 8px 16px;
+  font-size: 14px;
+  font-weight: bold;
+  background-color: #ccd6eb;
+  border: 2px solid #ccd6eb;
+  border-radius: 4px;
+  cursor: pointer;
+  color: black;
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+}
+
+.button:hover {
+  background-color: #a3c3d9;
+}
+</style>
