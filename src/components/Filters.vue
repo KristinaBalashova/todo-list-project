99efc4c3645b1119118f3f73983233e4715607a9
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-11-12 23:45:13 +0100

    Add filters

---
+++ src/components/Filters.vue
@@ -0,0 +1,50 @@
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'Filters',
+  computed: {
+    ...mapGetters('filter', ['filter']),
+  },
+  methods: {
+    ...mapActions('filter', ['setFilter']),
+  },
+};
+</script>
+
+<template>
+  <div class="filters">
+    <p class="option" :class="{ active: filter === 'all' }" @click="setFilter('all')">Все</p>
+    <p class="option" :class="{ active: filter === 'active' }" @click="setFilter('active')">
+      Активные
+    </p>
+    <p class="option" :class="{ active: filter === 'completed' }" @click="setFilter('completed')">
+      Завершенные
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.filters {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+
+.option {
+  cursor: pointer;
+  padding: 5px 10px;
+  font-weight: bold;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+}
+
+.option.active {
+  background-color: #007bff;
+  color: white;
+}
+
+.option:hover {
+  background-color: #a3c3d9;
+}
+</style>
