17416415837c71ec596b5468c4d09b78a595474b
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-04-29 18:55:20 +0200

    add: update font

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import { createPinia } from 'pinia'
 import router from './router'
+import './assets/main.css'
 
 const options = {
   position: "top-right",
