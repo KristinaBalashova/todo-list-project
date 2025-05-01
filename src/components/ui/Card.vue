d5a5d0e0e18f4c65692814b728809f222a9bc454
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-05-01 14:57:23 +0200

    add: basic project card templates

---
+++ src/components/ui/Card.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+defineProps({
+    loading: {
+        type: Boolean,
+        default: false
+    },
+    title: {
+        type: String,
+        default: "Card title"
+    },
+    subtitle: {
+        type: String,
+        default: "Subtitle"
+    },
+    text: {
+        type: String,
+        default: "Subtitle"
+    }
+})
+</script>
+
+<template>
+  <v-card :loading="loading" :title="title" :subtitle="subtitle" :text="text" class="ui-card">
+    <v-card-actions>
+      <slot name="action-button"/>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped>
+.ui-card {
+  width: 100%;
+  max-width: 400px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,15 +1,71 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="container">
-    Здесь будет страница со всеми проектами
+    <template v-for="(card, index) in cardData" :key="index">
+      <Card
+        :title="card.title"
+        :subtitle="card.subtitle"
+        :text="card.text"
+        :loading="false"            
+      >
+        <template #action-button>
+          <v-btn color="primary">Action</v-btn>
+        </template>
+      </Card>
+    </template>
   </div>
 </template>
+
+<script setup>
+import Card from '../components/ui/Card.vue';
+
+const cardData = [
+  {
+    title: 'Title 1',
+    subtitle: 'Subtitle 1',
+    text: 'This is the text for the first card.',
+  },
+  {
+    title: 'Title 2',
+    subtitle: 'Subtitle 2',
+    text: 'This is the text for the second card.',
+  },
+  {
+    title: 'Title 3',
+    subtitle: 'Subtitle 3',
+    text: 'This is the text for the third card.',
+  },
+  {
+    title: 'Title 4',
+    subtitle: 'Subtitle 4',
+    text: 'This is the text for the fourth card.',
+  }
+];
+</script>
 
 <style scoped>
 .container {
   width: 100%;
+  margin: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+}
+
+.container > * {
+  flex: 1 1 calc(50% - 10px); /* Каждая карточка занимает треть ширины с учетом отступа */
+  box-sizing: border-box;
+}
+
+@media (max-width: 1200px) {
+  .container > * {
+    flex: 1 1 calc(50% - 20px); /* Для экранов меньшего размера каждая карточка занимает половину ширины */
+  }
+}
+
+@media (max-width: 768px) {
+  .container > * {
+    flex: 1 1 100%; /* Для экранов мобильных устройств карточки занимают всю ширину */
+  }
 }
 </style>
