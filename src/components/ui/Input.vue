ca86bf5e80907e3520dc779f2784a0f0d8a510d8
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-05-20 19:55:31 +0200

    add: input design variant

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -4,6 +4,10 @@ defineProps({
     type: String,
     default: '',
   },
+  variant: {
+    type: String,
+    default: 'solo-filled', // default outlined undelined solo-filled solo-iverted
+  },
 });
 
 const modelValue = defineModel();
@@ -16,6 +20,8 @@ const modelValue = defineModel();
     @update:model-value="modelValue = $event"
     :placeholder="placeholder"
     class="custom-input"
+    :variant="variant"
+    clearable
   />
 </template>
 
