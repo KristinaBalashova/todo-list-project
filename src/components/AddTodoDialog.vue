e4ed66abbc0f758627098e0e985921664a66a623
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-05-22 16:05:40 +0200

    add: add todo dialog

---
+++ src/components/AddTodoDialog.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+import Dialog from './ui/Dialog.vue';
+import TodoForm from './TodoForm.vue';
+import Button from './ui/Button.vue';
+
+const dialogVisible = ref(false);
+</script>
+
+<template>
+  <div>
+    <Button color="primary" @click="dialogVisible = true">Добавить задачу</Button>
+    <Dialog v-model:visible="dialogVisible" title="Новая задача">
+      <template #content>
+        <TodoForm @close-dialog="dialogVisible = false"/>
+      </template>
+      <template #action-button>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<style scoped>
+</style>
