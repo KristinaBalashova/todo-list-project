f223910040b5086194de97536942691d464eee58
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-05 22:41:07 +0200

    add: edit form

--- src/views/TaskDrawer.vue
+++ src/views/TaskDrawer.vue
@@ -4,6 +4,15 @@ import { computed } from 'vue';
 import TodoForm from '../components/TodoForm.vue';
 import { useDrawerRoute } from '../composables/useDrawerRoute';
 import TodoView from '../components/TodoView.vue';
+import SaveForLaterBubble from '../components/SaveForLaterBubble.vue';
+import { useRoute } from 'vue-router';
+import { useTodos } from '../store';
+
+const route = useRoute();
+const store = useTodos();
+
+const taskId = route.params.id
+const tasktoEdit = store.todoById(taskId);
 
 const props = defineProps({
   modelValue: {
@@ -21,9 +30,9 @@ const {
 
 
 const title = computed(() => {
-  if (drawerMode === 'view') {
+  if (drawerMode.value === 'view') {
     return props.taskId ? `Задача #${props.taskId}` : 'Просмотр задачи';
-  } else if (drawerMode === 'create') {
+  } else if (drawerMode.value === 'create') {
     return 'Создание задачи';
   }
   return 'Задача';
@@ -34,6 +43,7 @@ const title = computed(() => {
 <template>
   <Drawer v-model="isDrawerVisible" :title="title">
     <TodoForm v-if="drawerMode === 'create'" />
+    <TodoForm v-else-if="drawerMode === 'edit'" :task="tasktoEdit" />
     <TodoView v-else-if="drawerMode === 'view'" :taskId="props.taskId" />
   </Drawer>
 </template>
